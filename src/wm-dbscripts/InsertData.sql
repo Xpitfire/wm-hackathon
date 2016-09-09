e2c9391800b455e5be8f3937f75bc6733676727e
0xLaura <LNeumayer@gmx.at>
2016-09-09 23:24:27 +0200

    Update InsertData.sql
    added 32 teams

--- src/wm-dbscripts/InsertData.sql
+++ src/wm-dbscripts/InsertData.sql
@@ -2,12 +2,29 @@ USE dbo;
 
 /* counrtyid, country */
 INSERT INTO `dbo`.`Team` VALUES 
-	('AUT', 'Österreich'), ('GER', 'Deutschland');
+	('AUT', 'Österreich'), ('GER', 'Deutschland'),
+	('ALB', 'Albanien'), ('BHS', 'Bahamas'), 
+	('CHN' 'China'), ('FIN', 'Finnland'),
+	('FRA', 'Frankreich'), ('GRC', 'Griechenland'),
+	('HKG', 'Hongkong'), ('IND', 'Indien'), 
+	('JPN', 'Japan'), ('CAN', 'Canada'),
+	('NOR', 'Norwegen'), ('NER', 'Niger'),
+	('PAK', 'Pakistan'), ('POL', 'Polen'), 
+	('SWE', 'Schweden'), ('SGP', 'Singapur'),
+	('ESP', 'Spanien'), ('THA', 'Thailand'),
+	('CZE', 'Tschechien'), ('TUN', 'Tunesien'),
+	('UKR', 'Urkaine'), ('VNM', 'Vietnam'),
+	('AND', 'Andora'), ('BEL', 'Belgien'), 
+	('BOL', 'Bolivien'), ('DMA', 'Dominica'),
+	('DNK', 'Dänemark'), ('ECU', 'Equador'),
+	('EST', 'Estland'), ('ECU', 'Ecuador');
 
 /* username, passwordhashvalue, firstname, lastname, isactive */
 INSERT INTO `dbo`.`User` VALUES 
 	('pony', '[B@64729b1e', 'Franz', 'Mustermann', 0),
-	('matrix', 'asdf', 'Susi', 'Musterfrau', 0);
+	('matrix', 'asdf', 'Susi', 'Musterfrau', 0),
+	('lili', 'hfhfhfhf', 'Lisa', 'Berger', 0),
+	('tobi', 'erewr', 'Tobias', 'Mayer', 0);
 
 /* id, team1, team2, place, date, goal1, goal2, wmstate */
 INSERT INTO `dbo`.`Game` VALUES 
