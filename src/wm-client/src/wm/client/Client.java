20cf54afbd5fc638c5a6d9f0b8d4c444a8f12ef9
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2016-09-10 00:49:04 +0200

    Removed project due to merge into existing wm-client.

--- src/wm-client/src/wm/client/Client.java
+++ src/wm-client/src/wm/client/Client.java
@@ -6,26 +6,44 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.List;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 import wm.lib.WmRmi;
 import wm.lib.model.User;
 
-public class Client {
-	
-	  public static void main(String[] args) {
+public class Client extends Application {
 
-	    String host_port = "localhost:18901";
-	    
-	    try {
-			WmRmi wmProxy = (WmRmi)Naming.lookup("rmi://" + host_port + "/WmRmi");
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root, 400, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		String host_port = "localhost:18901";
+
+		try {
+			WmRmi wmProxy = (WmRmi) Naming.lookup("rmi://" + host_port + "/WmRmi");
 			List<User> users = wmProxy.getUsers();
 			System.out.println("------------------------------");
-			
+
 			for (int i = 0; i < users.size(); i++) {
 				System.out.println("User: " + users.get(i).getUsername());
 			}
-	    } catch (MalformedURLException | RemoteException | NotBoundException e) {
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
 			e.printStackTrace();
 		}
-	  }
+
+		launch(args);
+	}
 
 }
